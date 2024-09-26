11f3baf7ebf93a2c77fc4b3d74d3959440bc0808
Kjetil Kyrkjerud Hårtveit <kjetil.kyrkjerud.hartveit@politiet.no>
2024-09-26 14:40:34 +0200

    feat: user delete and create

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express()
 const port = 3001
 
 app.use(cors())
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/api/user', async (req, res) => {
     // Get the client
@@ -22,6 +24,59 @@ app.get('/api/user', async (req, res) => {
     );
 
   res.send(results);
+})
+
+app.delete('/api/user/:userId/delete', async (req, res) => {
+  const userId = req.params.userId;
+
+  // Get the client
+  // Create the connection to database
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'newb',
+    password: 'Bombe656..'
+  });
+  
+  const [results] = await connection.execute(
+    'delete from user where id = ?',
+  [userId]
+  );
+
+  res.send(results.affectedRows > 0);
+})
+
+app.post('/api/user/create', async (req, res) => {
+  // Get the client
+  // Create the connection to database
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'newb',
+    password: 'Bombe656..'
+  });
+
+  const body = req.body;
+  
+  const [results] = await connection.execute(
+    'insert into user (first_name, last_name, email) values (?, ?, ?)',
+      [body['firstName'], body['lastName'], body['email']]
+  );
+
+  const anyAffectedRows = results.affectedRows > 0
+  if (anyAffectedRows) {
+    const user = await connection.query(`select * from User where id = ?`, [results.insertId])
+    res.send({
+      status: 'success',
+      message: 'Bruker lagt til.',
+      user: user[0][0]
+    });
+  } else {
+    res.send({
+      status: 'error',
+      message: 'Ingen bruker lagt til av en eller annen grunn.'
+    });
+  }
 })
 
 app.listen(port, () => {
